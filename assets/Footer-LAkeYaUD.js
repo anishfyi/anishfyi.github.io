51dbd84f2ddc7dabd5dc16cfdbc20b263906d6e5
anishfyi <anishfyi@users.noreply.github.com>
2025-03-07 16:13:05 +0000

    deploy: 562179be500077ff56e994b8c4e3921214fc52f5

--- assets/Footer-LAkeYaUD.js
+++
@@ -1,2 +0,0 @@
-import{j as t}from"./index-DF_TRCR0.js";const r=()=>t.jsx("footer",{className:"py-4 text-center text-gray-600 dark:text-gray-400 text-sm",children:t.jsxs("p",{children:["© ",new Date().getFullYear()," Anish Kr Singh"]})});export{r as default};
-//# sourceMappingURL=Footer-LAkeYaUD.js.map
