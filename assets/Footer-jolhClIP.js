c42ff37058491ef901b572881a372e93b1eeb0fa
anishfyi <anishfyi@users.noreply.github.com>
2025-03-07 14:29:11 +0000

    deploy: d6a5ff8596352ab1dc7a286ce238ca7e4c17fbd6

---
+++ assets/Footer-jolhClIP.js
@@ -0,0 +1,2 @@
+import{j as t}from"./index-D4t_Bg6L.js";const r=()=>t.jsx("footer",{className:"py-4 text-center text-gray-600 dark:text-gray-400 text-sm",children:t.jsxs("p",{children:["© ",new Date().getFullYear()," Anish Kr Singh"]})});export{r as default};
+//# sourceMappingURL=Footer-jolhClIP.js.map
