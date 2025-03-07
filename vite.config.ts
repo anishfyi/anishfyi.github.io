788a029fb4af24a4d674e2daff6dfc37c35d6445
Anish Kr Singh <59671878+anishfyi@users.noreply.github.com>
2025-03-07 19:22:34 +0530

    updated workflows

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/anishfyi.github.io/',
   plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: true
+  }
 })
