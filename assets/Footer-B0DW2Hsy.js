6e8c6afe50d2513437b04b698968f7dbe9a98d14
anishfyi <anishfyi@users.noreply.github.com>
2025-03-07 16:21:31 +0000

    deploy: 0fbeeb5ded6b4f2dd39844e0e15fcb507c345197

---
+++ assets/Footer-B0DW2Hsy.js
@@ -0,0 +1,2 @@
+import{j as t}from"./index-SNk01LSy.js";const r=()=>t.jsx("footer",{className:"py-4 text-center text-gray-600 dark:text-gray-400 text-sm",children:t.jsxs("p",{children:["© ",new Date().getFullYear()," Anish Kr Singh"]})});export{r as default};
+//# sourceMappingURL=Footer-B0DW2Hsy.js.map
