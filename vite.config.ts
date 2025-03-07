14ae242c80b344e164c2b9002635aa6de73fb596
Anish Kr Singh <59671878+anishfyi@users.noreply.github.com>
2025-03-07 19:28:39 +0530

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
