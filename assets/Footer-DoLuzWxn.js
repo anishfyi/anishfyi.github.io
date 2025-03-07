dedc44a950f97ab39feb0cb6e230112202005c17
anishfyi <anishfyi@users.noreply.github.com>
2025-03-07 15:25:50 +0000

    deploy: f0dbb226ba64897f27c253bb4fb3e08d68841b22

--- assets/Footer-DoLuzWxn.js
+++
@@ -1,2 +0,0 @@
-import{j as t}from"./index-DiWnPz1K.js";const r=()=>t.jsx("footer",{className:"py-4 text-center text-gray-600 dark:text-gray-400 text-sm",children:t.jsxs("p",{children:["© ",new Date().getFullYear()," Anish Kr Singh"]})});export{r as default};
-//# sourceMappingURL=Footer-DoLuzWxn.js.map
