d2f4b9bdf46f44877c7e0a8387a41551ee8dce57
anishfyi <anishfyi@users.noreply.github.com>
2025-05-08 07:09:28 +0000

    deploy: 8b8dae27657b767966c8b3c74058c9a5af119a30

--- assets/Footer-CK_lSyTI.js
+++
@@ -1,2 +0,0 @@
-import{j as t}from"./index-Fc9Fg9mb.js";import"./vendor-DEn4OjRg.js";const s=()=>t.jsx("footer",{className:"py-4 text-center text-gray-600 dark:text-gray-400 text-sm",children:t.jsxs("p",{children:["© ",new Date().getFullYear()," Anish Kr Singh"]})});export{s as default};
-//# sourceMappingURL=Footer-CK_lSyTI.js.map
